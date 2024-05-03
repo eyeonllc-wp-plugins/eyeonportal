63b9f3151aef96a7e0e23659d6b052fdb298de7a
Angrej Kumar <angrej.kumar@gmail.com>
2024-05-03 23:58:06 +0530

    retailers page 2 names wasn't loading on WP_SITE

--- eyeonportal.php
+++ eyeonportal.php
@@ -3,13 +3,13 @@
 Plugin Name: EyeOn Portal
 Plugin URI: https://eyeonllc.com/
 Description: Show Deals, Stores & Events of a Center from mycenterportal.com portal.
-Version: 0.0.95
+Version: 0.0.96
 Author: EyeOn LLC
 Author URI: https://eyeonllc.com/
 Licence: GPLv2 or later
 */
 
-define('THREEJS_MAP_VERSION', '0.0.129');
+define('THREEJS_MAP_VERSION', '0.0.130');
 
 require 'plugin-update-checker/plugin-update-checker.php';
 use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
