d12dfc1707253446c6d132c7094443b9b611cddd
Angrej Kumar <angrej.kumar@gmail.com>
2024-05-18 02:16:19 +0530

    5 new amenities added to map

--- eyeonportal.php
+++ eyeonportal.php
@@ -3,13 +3,13 @@
 Plugin Name: EyeOn Portal
 Plugin URI: https://eyeonllc.com/
 Description: Show Deals, Stores & Events of a Center from mycenterportal.com portal.
-Version: 0.0.99
+Version: 0.0.100
 Author: EyeOn LLC
 Author URI: https://eyeonllc.com/
 Licence: GPLv2 or later
 */
 
-define('THREEJS_MAP_VERSION', '0.0.134');
+define('THREEJS_MAP_VERSION', '0.0.136');
 
 require 'plugin-update-checker/plugin-update-checker.php';
 use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
--- readme.txt
+++ readme.txt
@@ -1,6 +1,6 @@
 === Plugin Name ===
 
-Stable tag:      0.0.99
+Stable tag:      0.0.100
 Tested up to:    6.3
 Licence:         GPLv2 or later
 Licence URI:     http://www.gnu.org/licenses/gpl-2.0.html
