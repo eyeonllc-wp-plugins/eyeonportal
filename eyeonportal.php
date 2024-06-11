e47c42d37f92c2af2b9aa6c476b20a36be539119
Angrej Kumar <angrej.kumar@gmail.com>
2024-06-11 23:38:33 +0530

    phone number formatted in retailers modal in map, map package updated

--- eyeonportal.php
+++ eyeonportal.php
@@ -3,13 +3,13 @@
 Plugin Name: EyeOn Portal
 Plugin URI: https://eyeonllc.com/
 Description: Show Deals, Stores & Events of a Center from mycenterportal.com portal.
-Version: 0.0.105
+Version: 0.0.106
 Author: EyeOn LLC
 Author URI: https://eyeonllc.com/
 Licence: GPLv2 or later
 */
 
-define('THREEJS_MAP_VERSION', '0.0.137');
+define('THREEJS_MAP_VERSION', '0.0.138');
 
 require 'plugin-update-checker/plugin-update-checker.php';
 use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
