edd5ea1c305a1a5e1e6011165892b53754f81ee2
Angrej Kumar <angrej.kumar@gmail.com>
2024-09-15 02:44:05 +0530

    new map released, retailer info updates

--- eyeonportal.php
+++ eyeonportal.php
@@ -3,13 +3,13 @@
 Plugin Name: EyeOn Portal
 Plugin URI: https://eyeonllc.com/
 Description: Show Deals, Stores & Events of a Center from mycenterportal.com portal.
-Version: 0.1.2
+Version: 0.1.3
 Author: EyeOn LLC
 Author URI: https://eyeonllc.com/
 Licence: GPLv2 or later
 */
 
-define('THREEJS_MAP_VERSION', '0.1.13');
+define('THREEJS_MAP_VERSION', '0.1.14');
 
 require 'plugin-update-checker/plugin-update-checker.php';
 use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
