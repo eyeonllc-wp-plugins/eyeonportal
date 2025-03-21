2193ac5ce845f935c47726ae621ff8ac24eaac3a
Angrej Kumar <angrej.kumar@gmail.com>
2025-03-21 13:41:48 +0530

    working on event custom dates

--- elementor/widgets/events/render.php
+++ elementor/widgets/events/render.php
@@ -6,10 +6,6 @@ $fields = [
   'external_event_new_tab',
   'event_title',
   'event_excerpt',
-  'event_date',
-  'event_display_date',
-  'event_time',
-  'event_ongoing_dates',
   'event_category',
   'no_results_found_text',
 ];
@@ -265,29 +261,26 @@ $unique_id = uniqid();
                 <img src="${event.media.url}" alt="${event.title}" />
               </div>
               <div class="event-content">
-                ${ settings.event_title ? `<h3 class="event-title">${event.title}</h3>` : '' }
-                ${ settings.event_excerpt? `<p class="event-excerpt">${event.short_description}</p>` : '' }
-                ${ (settings.event_date === 'show' || settings.event_time === 'show') ? `
+                ${ settings.event_title ? `<h3 class="event-title">${event.title}</h3>` : ``}
+                ${ settings.event_excerpt ? `<p class="event-excerpt">${event.short_description}</p>` : ``}
+                ${ (event.show_date || event.show_event_time) ? `
                   <div class="metadata">
-                    ${ settings.event_date && (!event.ongoing_event || (event.ongoing_event && settings.event_ongoing_dates)) ? `
+                    ${ event.show_date ? `
                       <div class="date">
                         <i class="far fa-calendar"></i>
-                        ${ settings.event_display_date == 'upcoming' ? `
-                          <span>${event.datesStr}</span>
-                        ` : `
-                          <span>${event.formatted_start_date} - ${event.formatted_end_date}</span>
-                        `}
+                        ${ event.show_upcoming_date ? `<span>${event.datesStr}</span>` : ``}
+                        ${ event.show_date_range ? `<span>${event.formatted_start_date} - ${event.formatted_end_date}</span>` : ``}
                       </div>
-                    `: '' }
-                    ${(settings.event_time && !event.is_all_day_event) ? `
+                    `:``}
+                    ${ event.show_event_time && !event.is_all_day_event ? `
                       <div class="time">
                         <i class="far fa-clock"></i>
                         <span>${eyeonFormatTime(event.start_time)} - ${eyeonFormatTime(event.end_time)}</span>
                       </div>
-                    ` : '' }
+                    `:``}
                   </div>
-                  `: '' }
-                </div>
+                `:``}
+              </div>
             </a>
           `);
           eventsList.append(eventItem);
